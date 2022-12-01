18a4c3935ff030f41cd9b35eeb1438c30896ce34
fernandosilvam10 <fernando.silva.fsm10@gmail.com>
2022-12-01 10:58:58 -0600

    En el programa temp hicimos 1er compilacion

--- TAREA_1.py
+++ TAREA_1.py
@@ -5,12 +5,13 @@ Created on Thu Dec  1 08:28:48 2022
 @author: ferna
 """
 import mnist_loader 
-import network4
+import network
+import pickle
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 training_data = list(training_data)
 test_data = list(test_data)
 
-net=network4.Network([784,30,10])
+net=network.Network([784,30,10])
 net.RMSPROP (training_data, 30, 10, 3.0, test_data=test_data)
